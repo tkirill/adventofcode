2b7db86cb9bfee50c4e9d623355688c742dfcb63
Kirill Teplinskiy <tkirill128@gmail.com>
2021-12-04 00:24:35 +0500

    AoC 2021 Day 3

---
+++ 2021/3.py
@@ -0,0 +1,62 @@
+from collections import Counter
+from functools import reduce
+
+
+def read_input():
+    with open('3_input.txt') as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+def star1():
+    lines = read_input()
+    counters = [Counter(bits) for bits in zip(*lines)]
+    gamma, epsilon = [int(''.join(c.most_common()[pos][0] for c in counters), base=2) for pos in [0, -1]]
+    print(gamma * epsilon)
+
+
+print('Star 1:')
+star1()
+
+
+def count_most_common(lines):
+    count_zeroes = [0] * len(lines[0])
+    for line in lines:
+        for i, c in enumerate(line):
+            if c == '0':
+                count_zeroes[i] += 1
+    result = [0] *len(count_zeroes)
+    for i, c in enumerate(count_zeroes):
+        is_one = c < (len(lines) / 2)
+        if is_one:
+            result[i] = 1
+    return result
+
+
+def find_oxy(lines):
+    for i in range(len(lines[0])):
+        cnts = Counter(line[i] for line in lines)
+        b = '1' if cnts['1'] >= cnts['0'] else '0'
+        tmp = [line for line in lines if line[i] == b]
+        if len(tmp) == 1:
+            return int(''.join(tmp[0]), base=2)
+        lines = tmp
+
+
+def find_co2(lines):
+    for i in range(len(lines[0])):
+        cnts = Counter(line[i] for line in lines)
+        b = '1' if cnts['1'] < cnts['0'] else '0'
+        tmp = [line for line in lines if line[i] == b]
+        if len(tmp) == 1:
+            return int(''.join(tmp[0]), base=2)
+        lines = tmp
+
+
+def star2():
+    lines = read_input()
+    oxy, co2 = find_oxy(lines), find_co2(lines)
+    print(oxy, co2)
+    print(oxy * co2)
+
+
+star2()
