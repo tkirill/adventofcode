7db1a375cbbc802291bc0734b163b6d3ed125772
Kirill Teplinskiy <tkirill128@gmail.com>
2021-12-04 23:45:56 +0500

    AoC 2021 Day 4

---
+++ 2021/4.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple
+from utils import *
+
+
+def read_input() -> Tuple[List[int], List[List[int]]]:
+    first_line, *blocks = splitfalse(readlines('4_input.txt'))
+    return parse1d(first_line[0], ','), [parse2d(b) for b in blocks]
+
+
+def bingo(board):
+    won = False
+    def move(number):
+        nonlocal won
+        if won:
+            return None
+        for row in board:
+            for col in range(len(row)):
+                if row[col] == number:
+                    row[col] = None
+                    if not any(row) or not any(column(board, col)):
+                        won = True
+                        return sum(x for row in board for x in row if x) * number
+        return None
+    return move
+
+
+numbers, boards = read_input()
+players, scores = [bingo(b) for b in boards], []
+for x in numbers:
+    scores.extend(filter(None, (p(x) for p in players)))
+
+print('Star 1:', scores[0])
+print('Star 2:', scores[-1])
---
+++ 2021/utils.py
@@ -0,0 +1,33 @@
+from typing import Callable, Iterable, Optional, List, TypeVar
+
+
+T = TypeVar('T')
+
+
+def readlines(filename) -> List[str]:
+    with open(filename) as f:
+        return f.read().splitlines()
+
+
+def parse1d(line: str, sep: Optional[str]=None) -> List[int]:
+    return [int(x) for x in line.split(sep)]
+
+
+def parse2d(lines: Iterable[str], sep: Optional[str]=None) -> List[List[int]]:
+    return [parse1d(line, sep) for line in lines]
+
+
+def splitfalse(items: Iterable[T], pred: Optional[Callable[[T],bool]]=None) -> List[List[T]]:
+    buf = []
+    for item in items:
+        if pred and pred(item) or not pred and item:
+            buf.append(item)
+        elif buf:
+            yield buf
+            buf = []
+    if buf:
+        yield buf
+
+
+def column(a: List[List[T]], col: int) -> List[T]:
+    return [row[col] for row in a]
