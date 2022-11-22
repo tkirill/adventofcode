2c01f3cd23fe24c1e431be555e72643c7f1b80ab
Kirill Teplinskiy <tkirill128@gmail.com>
2022-11-22 18:21:30 +0500

    2015 day 1 & day 2

---
+++ 2015/python/01_Not_Quite_Lisp.py
@@ -0,0 +1,11 @@
+def walk(s):
+    floor = 0
+    for c in s:
+        floor += 1 if c == '(' else -1
+        yield floor
+
+line = open('01_input.txt').readline().strip()
+floors = list(walk(line))
+
+print('Star 1:', floors[-1])
+print('Star 2:', floors.index(-1)+1)
---
+++ 2015/python/02.py
@@ -0,0 +1,14 @@
+lines = [x.strip() for x in open('02_input.txt').readlines()]
+presents = [tuple(int(xx) for xx in x.split('x')) for x in lines]
+
+total = 0
+for p in presents:
+    sides = [p[0]*p[1], p[1]*p[2], p[0]*p[2]]
+    total += sum(sides) * 2 + min(sides)
+print('Star 1:', total)
+
+total = 0
+for p in presents:
+    sides = [p[0]+p[1], p[1]+p[2], p[0]+p[2]]
+    total += min(sides)*2 + p[0]*p[1]*p[2]
+print('Star 2:', total)
