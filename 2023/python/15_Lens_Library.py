3716568831f777c55dc78a4077aa1a0351103f16
Kirill Teplinskiy <tkirill128@gmail.com>
2023-12-15 19:42:46 +0500

    Add 2023 day 15

---
+++ 2023/python/15_Lens_Library.py
@@ -0,0 +1,32 @@
+from aoc import *
+from dataclasses import dataclass
+import more_itertools as mtls
+import functools as ftls
+
+
+@dataclass
+class HASHable:
+    val: str
+
+    def __hash__(self):
+        return ftls.reduce(lambda h, c: ((h + ord(c)) * 17) % 256, self.val, 0)
+
+
+steps = read(sep=",")[0]
+print("Star 1:", sum(hash(HASHable(s)) for s in steps))
+
+
+steps = parselines(steps, sep="[=\-]")
+boxes = {}
+for label, focal in steps:
+    label = HASHable(label)
+    if focal == "":
+        boxes.pop(label, None)
+    else:
+        boxes[label] = focal
+
+total = 0
+buckets = mtls.bucket(boxes.items(), key=lambda x: hash(x[0]))
+for h in buckets:
+    total += sum((h + 1) * (i + 1) * lens[1] for i, lens in enumerate(buckets[h]))
+print("Star 2:", total)
