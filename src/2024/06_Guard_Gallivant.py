4c98b00fb93a8354d5e3558eb16c079d718454c0
Kirill Teplinskiy <tkirill128@gmail.com>
2024-12-06 15:45:19 +0500

    refactor 2024.06

--- src/2024/06_Guard_Gallivant.py
+++ src/2024/06_Guard_Gallivant.py
@@ -8,33 +8,30 @@ import more_itertools as mitls
 
 def walk(field, cur):
     walker = GridWalker(cur, velocity=field.grid.delta_up)
+    yield walker
     while True:
         n = walker.step()
         if n.pos not in field:
             break
-        if field[n.pos] != '.':
-            walker = walker.rotatelr('R')
-        else:
+        if field[n.pos] != '#':
             walker = n
             yield walker
+        else:
+            walker = walker.rotatelr('R')
 
 
 field = Field(read(parse=list))
 start = [p for p, v in field.cellsv() if v == '^'][0]
-field[start] = '.'
-visited = {start}
-for w in walk(field, start):
-    visited.add(w.pos)
+visited = {w.pos for w in walk(field, start)}
 print('Star 1:', len(visited))
 
 
 result = 0
-for p, v in field.cellsv():
-    if p == start or v != '.':
+for p in visited:
+    if p == start:
         continue
-    nf = Field(read(parse=list))
-    nf[start] = '.'
-    nf[p] = '#'
-    if algo.detect_cycle(walk(nf, start)) is not None:
+    field[p] = '#'
+    if algo.detect_cycle(walk(field, start)) is not None:
         result += 1
+    field[p] = '.'
 print('Star 2:', result)
