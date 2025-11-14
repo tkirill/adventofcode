90d30a18964324f221241b5bff20d783f880852f
Kirill Teplinskiy <tkirill128@gmail.com>
2025-11-15 02:18:05 +0500

    Simplify 2023-01

--- src/aoc/year2023/day_01_Trebuchet.py
+++ src/aoc/year2023/day_01_Trebuchet.py
@@ -1,35 +1,26 @@
+from typing import Sequence
+
 from aoc.io import readlines
-from typing import Sequence, Iterable
-from itertools import chain
 
 
 DIGITS = '123456789'
 DIGIT_WORDS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+ALL_DIGITS = list(DIGITS) + DIGIT_WORDS
+DIGIT_TO_INT = {d: DIGITS.index(d)+1 for d in DIGITS} | {d: DIGIT_WORDS.index(d)+1 for d in DIGIT_WORDS}
 
 
-def find_first_positions(line: str, digits: Sequence[str]) -> Iterable[tuple[int, int]]:
-    return [(line.find(d), i+1) for i, d in enumerate(digits) if d in line]
-
-
-def find_last_positions(line: str, digits: Sequence[str]) -> Iterable[tuple[int, int]]:
-    return [(line.rfind(d), i+1) for i, d in enumerate(digits) if d in line]
+def find_first_and_last_digits(line: str, digits: Sequence[str]) -> int:
+    first = min([d for d in digits if d in line], key=line.find)
+    last = max([d for d in digits if d in line], key=line.rfind)
+    return int(f'{DIGIT_TO_INT[first]}{DIGIT_TO_INT[last]}')
 
 
 def star1():
-    total = 0
-    for l in readlines(year=2023, day=1):
-        first, last = min(find_first_positions(l, DIGITS)), max(find_last_positions(l, DIGITS))
-        total += first[1]*10 + last[1]
-    return total
+    return sum(find_first_and_last_digits(line, DIGITS) for line in readlines(year=2023, day=1))
 
 
 def star2():
-    total = 0
-    for l in readlines(year=2023, day=1):
-        first = min(chain(find_first_positions(l, DIGITS), find_first_positions(l, DIGIT_WORDS)))
-        last = max(chain(find_last_positions(l, DIGITS), find_last_positions(l, DIGIT_WORDS)))
-        total += first[1]*10 + last[1]
-    return total
+    return sum(find_first_and_last_digits(line, ALL_DIGITS) for line in readlines(year=2023, day=1))
 
 
 if __name__ == '__main__':
