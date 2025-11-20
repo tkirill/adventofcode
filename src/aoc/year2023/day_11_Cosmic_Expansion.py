33168e8f8f6734014d9d673baa4a744473144699
Kirill Teplinskiy <tkirill128@gmail.com>
2025-11-21 02:02:08 +0500

    Optimize 2023 day 10 even more

--- src/aoc/year2023/day_11_Cosmic_Expansion.py
+++ src/aoc/year2023/day_11_Cosmic_Expansion.py
@@ -8,6 +8,16 @@ from aoc.vec2 import Vec2
 from aoc import grid
 
 
+def find_galaxies(universe: Board[str]) -> tuple[list[Vec2], list[int], list[bool]]:
+    galaxies, is_empty_row, is_empty_col = [], [1] * universe.height, [1] * universe.width
+    for p, v in board.cellsv(universe):
+        if v == '#':
+            galaxies.append(p)
+            is_empty_row[p.y] = 0
+            is_empty_col[p.x] = 0
+    return galaxies, list(accumulate(is_empty_row)), list(accumulate(is_empty_col))
+
+
 def distance(a: Vec2, b: Vec2, empty_rows: list[int], empty_cols: list[int], coeff: int) -> int:
     result = grid.mdist(a, b)
     l, r = min(a.x, b.x), max(a.x, b.x)
@@ -19,17 +29,13 @@ def distance(a: Vec2, b: Vec2, empty_rows: list[int], empty_cols: list[int], coe
 
 def star1():
     universe = Board(read(2023, 11, sep=None, parse=list))
-    galaxies = [p for p, v in board.cellsv(universe) if v == '#']
-    empty_rows = list(accumulate(int(all(v == '.' for _, v in row)) for row in board.rowsv(universe)))
-    empty_cols = list(accumulate(int(all(v == '.' for _, v in row)) for row in board.colsv(universe)))
+    galaxies, empty_rows, empty_cols = find_galaxies(universe)
     return sum(distance(a, b, empty_rows, empty_cols, 2) for a, b in combinations(galaxies, 2))
 
 
 def star2():
     universe = Board(read(2023, 11, sep=None, parse=list))
-    galaxies = [p for p, v in board.cellsv(universe) if v == '#']
-    empty_rows = list(accumulate(int(all(v == '.' for _, v in row)) for row in board.rowsv(universe)))
-    empty_cols = list(accumulate(int(all(v == '.' for _, v in row)) for row in board.colsv(universe)))
+    galaxies, empty_rows, empty_cols = find_galaxies(universe)
     return sum(distance(a, b, empty_rows, empty_cols, 1_000_000) for a, b in combinations(galaxies, 2))
 
 
