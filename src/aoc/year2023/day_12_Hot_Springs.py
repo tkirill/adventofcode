d9a2fee517373b22284010d23b4e43dcb6b2ea50
Kirill Teplinskiy <tkirill128@gmail.com>
2025-11-21 16:37:27 +0500

    Minor optimization for 2023 day 12

--- src/aoc/year2023/day_12_Hot_Springs.py
+++ src/aoc/year2023/day_12_Hot_Springs.py
@@ -3,33 +3,33 @@ from functools import cache
 
 
 @cache
-def arrangements(springs: str, groups: tuple[int]) -> int:
+def arrangements(springs: str, groups: tuple[int], groups_len: int) -> int:
     if not springs:
         return not groups
     if not groups:
         return springs.find('#') == -1
     if len(springs) < groups[0]:
         return 0
-    if len(springs) < sum(groups) + len(groups) - 1:
+    if len(springs) < groups_len + len(groups) - 1:
         return 0
     if springs[0] == '.':
-        return arrangements(springs.lstrip('.'), groups)
+        return arrangements(springs.lstrip('.'), groups, groups_len)
     total = 0
     if springs[0] == '?':
-        total += arrangements(springs[1:], groups)
+        total += arrangements(springs[1:], groups, groups_len)
     if springs.find('.', 0, groups[0]) == -1 and (len(springs) == groups[0] or springs[groups[0]] != '#'):
-        total += arrangements(springs[groups[0]+1:], groups[1:])
+        total += arrangements(springs[groups[0]+1:], groups[1:], groups_len-groups[0])
     return total
 
 
 def star1():
     records = read(2023, 12, sep=r' |,')
-    return sum(arrangements(springs, tuple(groups)) for springs, *groups in records)
+    return sum(arrangements(springs, tuple(groups), sum(groups)) for springs, *groups in records)
 
 
 def star2():
     records = [('?'.join([s]*5), tuple(g*5)) for s, *g in read(2023, 12, sep=r' |,')]
-    return sum(arrangements(springs, groups) for springs, groups in records)
+    return sum(arrangements(springs, groups, sum(groups)) for springs, groups in records)
 
 
 if __name__ == '__main__':
