a4586670be14b67e0bbb3e68273ccd43bc55f414
Kirill Teplinskiy <tkirill128@gmail.com>
2025-11-23 02:05:53 +0500

    Generalize BFS implementation

--- src/aoc/algorithm.py
+++ src/aoc/algorithm.py
@@ -2,18 +2,44 @@ from typing import Iterable, Callable
 from collections import deque
 
 
-def bfs_initial[TState](initial: Iterable[TState], near: Callable[[TState], Iterable[TState]], track_visited: bool=True):
+def filter_visited[TState](
+        initial: Iterable[TState],
+        near: Callable[[TState], Iterable[TState]]) -> Callable[[TState], Iterable[TState]]:
+    visited = set(initial)
+
+    def _near[TState](s: TState) -> Iterable[TState]:
+        for n in near(s):
+            if n not in visited:
+                visited.add(n)
+                yield n
+    
+    return _near
+
+
+def bfs_initial[TState](
+        initial: list[TState],
+        near: Callable[[TState], Iterable[TState]],
+        track_visited: bool=True) -> Iterable[tuple[TState, int]]:
+    if track_visited:
+        near = filter_visited(initial, near)
+    yield from _bfs_impl(initial, near)
+
+
+def bfs[TState](
+        initial: list[TState],
+        near: Callable[[TState], Iterable[TState]],
+        track_visited: bool=True):
+    yield from bfs_initial([initial], near, track_visited)
+
+
+def _bfs_impl[TState](
+        initial: Iterable[TState],
+        near: Callable[[TState], Iterable[TState]]) -> Iterable[tuple[TState, int]]:
     q = deque(initial)
-    visited = set(q) if track_visited else None
     curdist = 0
     while q:
         for _ in range(len(q)):
             cur = q.popleft()
             yield cur, curdist
-            for n in near(cur):
-                if not track_visited or n not in visited:
-                    q.append(n)
-                    if track_visited:
-                        visited.add(n)
+            q.extend(near(cur))
         curdist += 1
-    
