6ba88f17eb80889e3e20a1f8e20a81d479b03900
Kirill Teplinskiy <tkirill128@gmail.com>
2025-11-28 00:36:10 +0500

    Refactor 2023 day 15 to dict

--- src/aoc/year2023/day_15_Lens_Library.py
+++ src/aoc/year2023/day_15_Lens_Library.py
@@ -10,24 +10,10 @@ def compute_hash(s: str) -> int:
     return total
 
 
-def remove_lens(box: list[tuple[str, int]], label: str):
-    p = next((i for i, (l, _) in enumerate(box) if l == label), None)
-    if p is not None:
-        del box[p]
-
-
-def append_lens(box: list[tuple[str, int]], label: str, focal: int):
-    p = next((i for i, (l, _) in enumerate(box) if l == label), None)
-    if p is not None:
-        box[p] = (label, focal)
-        return
-    box.append((label, focal))
-
-
-def focusing_power(boxes: list[list[tuple[str, int]]]) -> int:
+def focusing_power(boxes: list[dict[str, int]]) -> int:
     total = 0
     for i, box in enumerate(boxes):
-        for j, (_, f) in enumerate(box):
+        for j, (_, f) in enumerate(box.items()):
             total += (i + 1) * (j + 1) * f
     return total
 
@@ -39,15 +25,15 @@ def star1():
 
 def star2():
     steps = read(year=2023, day=15, sep=',', parse=str)[0]
-    boxes = [[] for _ in range(256)]
+    boxes = [dict() for _ in range(256)]
     for step in steps:
         if step[-1] == '-':
             h = compute_hash(step[:-1])
-            remove_lens(boxes[h], step[:-1])
+            boxes[h].pop(step[:-1], None)
             continue
         label, focal = step.split('=')
         h = compute_hash(label)
-        append_lens(boxes[h], label, int(focal))
+        boxes[h][label] = int(focal)
     return focusing_power(boxes)
 
 
