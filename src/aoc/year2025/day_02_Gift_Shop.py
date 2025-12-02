c149db2a1821e05fd6900f6a39994794742b698b
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-02 23:22:23 +0500

    Speed up 2025 day 2

--- src/aoc/year2025/day_02_Gift_Shop.py
+++ src/aoc/year2025/day_02_Gift_Shop.py
@@ -9,7 +9,8 @@ def star1():
     for a, b in ranges:
         for i in range(a, b+1):
             s = str(i)
-            if len(s) % 2 == 0 and s[:len(s)//2] == s[len(s)//2:]:
+            mid = len(s)//2
+            if s[:mid] == s[mid:]:
                 total += i
     return total
 
@@ -17,12 +18,9 @@ def star1():
 def star2():
     ranges = itertools.batched(read(2025, 2, sep=r',|-')[0], 2)
     total = 0
-    visited = set()
     for a, b in ranges:
         for i in range(a, b+1):
             s = str(i)
-            for j in range(1,len(s)):
-                if len(s) % j == 0 and (len(s) // j) >= 2 and s not in visited and s == (s[:j] * (len(s) // j)):
-                    total += i
-                    visited.add(s)
+            if s in (s + s)[1:-1]:
+                total += i
     return total
