d89461c0819dee95de3694f95ac9e1dba7c12578
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-03 11:19:52 +0500

    Refactor 2025 day 3

--- src/aoc/year2025/day_03_Lobby.py
+++ src/aoc/year2025/day_03_Lobby.py
@@ -1,78 +1,27 @@
-import itertools, functools, more_itertools, math
-
-from aoc.io import read, readlines, allints
+from aoc.io import readlines
 from aoc import colutils
 
 
-def biggest(bank: list[int]):
-    for fd in range(9, 0, -1):
-        fdi = colutils.index(bank, fd)
-        if fdi == -1:
+def joltage(bank: list[int], batteries: int, start: int=0) -> int:
+    for b in range(9, 0, -1):
+        i = colutils.index(bank, b, start)
+        if i == -1:
             continue
-        for sd in range(9, 0, -1):
-            sdi = colutils.index(bank, sd, fdi+1)
-            if sdi != -1:
-                return fd*10+sd
-
-
-def biggest12(bank: list[int]) -> int:
-    for fd in range(9, 0, -1):
-        fdi = colutils.index(bank, fd)
-        if fdi == -1:
-            continue
-        for sd in range(9, 0, -1):
-            sdi = colutils.index(bank, sd, fdi+1)
-            if sdi == -1:
-                continue
-            for sd3 in range(9, 0, -1):
-                sd3i = colutils.index(bank, sd3, sdi+1)
-                if sd3i == -1:
-                    continue
-                for sd4 in range(9, 0, -1):
-                    sd4i = colutils.index(bank, sd4, sd3i+1)
-                    if sd4i == -1:
-                        continue
-                    for sd5 in range(9, 0, -1):
-                        sd5i = colutils.index(bank, sd5, sd4i+1)
-                        if sd5i == -1:
-                            continue
-                        for sd6 in range(9, 0, -1):
-                            sd6i = colutils.index(bank, sd6, sd5i+1)
-                            if sd6i == -1:
-                                continue
-                            for sd7 in range(9, 0, -1):
-                                sd7i = colutils.index(bank, sd7, sd6i+1)
-                                if sd7i == -1:
-                                    continue
-                                for sd8 in range(9, 0, -1):
-                                    sd8i = colutils.index(bank, sd8, sd7i+1)
-                                    if sd8i == -1:
-                                        continue
-                                    for sd9 in range(9, 0, -1):
-                                        sd9i = colutils.index(bank, sd9, sd8i+1)
-                                        if sd9i == -1:
-                                            continue
-                                        for sd10 in range(9, 0, -1):
-                                            sd10i = colutils.index(bank, sd10, sd9i+1)
-                                            if sd10i == -1:
-                                                continue
-                                            for sd11 in range(9, 0, -1):
-                                                sd11i = colutils.index(bank, sd11, sd10i+1)
-                                                if sd11i == -1:
-                                                    continue
-                                                for sd12 in range(9, 0, -1):
-                                                    sd12i = colutils.index(bank, sd12, sd11i+1)
-                                                    if sd12i != -1:
-                                                        return sd12 + sd11*10 + sd10*100 + sd9*1000+ sd8*10000 + sd7*100_000 + sd6*1_000_000 + sd5*10_000_000 + sd4*100_000_000 + sd3*1_000_000_000+sd*10_000_000_000+fd*100_000_000_000
+        if batteries == 1:
+            return b
+        j = joltage(bank, batteries-1, i+1)
+        if j != -1:
+            return b * 10**(batteries-1) + j
+    return -1
 
 
 def star1():
     banks = readlines(2025, 3)
     banks = [[int(x) for x in b] for b in banks]
-    return sum(biggest(b) for b in banks)
+    return sum(joltage(b, 2) for b in banks)
 
 
 def star2():
     banks = readlines(2025, 3)
     banks = [[int(x) for x in b] for b in banks]
-    return sum(biggest12(b) for b in banks)
+    return sum(joltage(b, 12) for b in banks)
