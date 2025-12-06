043941237f699d269de7e651d2633410f6acf298
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-06 16:21:50 +0500

    Move 2023 day 18 star 2 to new architecture

--- src/aoc/year2023/day_18_Lavaduct_Lagoon.py
+++ src/aoc/year2023/day_18_Lavaduct_Lagoon.py
@@ -1,25 +1,29 @@
-from typing import Iterable
-
 from aoc.io import read
 from aoc.vec2 import Vec2
 from aoc import grid, board
 
 
-def dig(plan: Iterable[list[str, int, str]]) -> board.Board[str]:
+DIRECTIONS = [grid.RIGHT, grid.DOWN, grid.LEFT, grid.UP]
+
+
+def dig(plan: list[tuple[int, int]]) -> board.Board[str]:
     cur = Vec2(0, 0)
     border = [cur]
-    for direction, distance, _ in plan:
-        delta = grid.direction(direction)
-        border.extend(grid.beam(cur, delta, distance, skip_start=True))
+    for direction, distance in plan:
+        border.append(cur + direction*distance)
         cur = border[-1]
     return border
 
 
 def star1():
-    plan = read(2023, 18)
+    data = read(2023, 18)
+    plan = [(grid.direction(_dir), dist) for _dir, dist, _ in data]
     edge = dig(plan)
     return grid.pick_theorem(edge, include_edge=True)
     
 
 def star2():
-    pass
+    data = read(2023, 18)
+    plan = [(int(color[2:-2], 16), DIRECTIONS[int(color[-2])]) for _, _, color in data]
+    edge = dig(plan)
+    return grid.pick_theorem(edge, include_edge=True)
