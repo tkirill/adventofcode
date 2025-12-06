515f83f40ccadcfb66bd0b031b96e45fc8860b16
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-06 12:32:51 +0500

    Refactor 2025 day 6

--- src/aoc/year2025/day_06_Trash_Compactor.py
+++ src/aoc/year2025/day_06_Trash_Compactor.py
@@ -6,14 +6,7 @@ from aoc import board
 
 def star1():
     items = read(2025, 6)
-    total = 0
-    for *a, op in zip(*items):
-        match op:
-            case '+':
-                total += sum(a)
-            case '*':
-                total += math.prod(a)
-    return total
+    return sum(sum(a) if op == '+' else math.prod(a) for *a, op in zip(*items))
 
 
 def star2():
@@ -22,17 +15,13 @@ def star2():
     args = []
 
     for col in board.colsv(items, _reversed=True):
-        vals = ''.join(v for _, v in col)
-        if all(x == ' ' for x in vals):
+        vals = ''.join(v for _, v in col).strip()
+        if not vals:
             continue
-        if '+' not in vals and '*' not in vals:
+        if vals[-1] not in ['+', '*']:
             args.append(int(vals))
             continue
         args.append(int(vals[:-1]))
-        match vals[-1]:
-            case '+':
-                total += sum(args)
-            case '*':
-                total += math.prod(args)
+        total += sum(args) if vals[-1] == '+' else math.prod(args)
         args.clear()
     return total
