385025d700dda1207cf6e77a0d687424a17675dc
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-07 20:53:44 +0500

    refactor 2025 day 7

--- src/aoc/year2025/day_07_Laboratories.py
+++ src/aoc/year2025/day_07_Laboratories.py
@@ -1,39 +1,31 @@
-from typing import Iterable
-from functools import partial, cache
-
 from aoc.io import read
-from aoc import board, grid, vec2, algorithm
-from aoc.vec2 import Vec2
+from aoc import board
 from aoc.board import Board
 
 
-@board.wrap_contains
-def tachyon_step(manifold: Board[str], pos: Vec2) -> Iterable[Vec2]:
-    if manifold[pos] != '^':
-        yield pos + grid.DOWN
-        return
-    yield pos + grid.LEFT
-    yield pos + grid.RIGHT
-
-
-def timelines(manifold: Board[str], pos: Vec2) -> int:
-    @cache
-    def timelines_step(pos: Vec2) -> int:
-        if manifold[pos] == '^':
-            return timelines_step(pos + grid.LEFT) + timelines_step(pos + grid.RIGHT)
-        if pos.y == manifold.height-1:
-            return 1
-        return timelines_step(pos+grid.DOWN)
-    return timelines_step(pos)
+def count_splitters(manifold: Board[str]) -> int:
+    timelines = [0] * len(manifold.values[0])
+    splitters = 0
+    for row in board.rowsv(manifold):
+        for pos, v in row:
+            if v == 'S':
+                timelines[pos.x] = 1
+                break
+            if v == '^' and timelines[pos.x]:
+                splitters += 1
+                timelines[pos.x-1] += timelines[pos.x]
+                timelines[pos.x+1] += timelines[pos.x]
+                timelines[pos.x] = 0
+    return splitters, sum(timelines)
 
 
 def star1():
     manifold = board.Board(read(2025, 7, sep=None, parse=list))
-    start = next(board.find(manifold, 'S'))
-    return sum(1 for p, _ in algorithm.bfs(start, partial(tachyon_step, manifold)) if manifold[p] == '^')
+    splitters, _ = count_splitters(manifold)
+    return splitters
 
 
 def star2():
     manifold = board.Board(read(2025, 7, sep=None, parse=list))
-    start = next(board.find(manifold, 'S'))
-    return timelines(manifold, start)
+    _, timelines = count_splitters(manifold)
+    return timelines
