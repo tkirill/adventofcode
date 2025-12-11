50d6897211c606bf96c76fcb0131f681e894bc97
Kirill Teplinskiy <tkirill128@gmail.com>
2025-12-11 18:55:53 +0500

    Approachin 2025 day 9 star 2

--- src/aoc/rectangle.py
+++ src/aoc/rectangle.py
@@ -12,3 +12,9 @@ class Rectangle:
     @classmethod
     def ylr(cls, y: int, l: int, r: int) -> Rectangle:
         return Rectangle(Vec2(l, y), r-l+1, 1)
+    
+    @classmethod
+    def by_corners(cls, u: Vec2, v: Vec2) -> Rectangle:
+        min_x, min_y = min(u.x, v.x), min(u.y, v.y)
+        max_x, max_y = max(u.x, v.x), max(u.y, v.y)
+        return Rectangle(Vec2(min_x, min_y), max_x-min_x, max_y-min_y)
--- src/aoc/year2025/day_09_Movie_Theater.py
+++ src/aoc/year2025/day_09_Movie_Theater.py
@@ -4,6 +4,7 @@ from aoc.io import read
 from aoc import grid, board
 from aoc.vec2 import Vec2
 from aoc.board import Board
+from aoc import rectangle
 
 
 def star1():
@@ -11,5 +12,15 @@ def star1():
     return max((abs(a[0]-b[0])+1)*(abs(a[1]-b[1])+1) for a, b in combinations(tiles, 2))
 
 
-def star2():
+def is_inside(tiles: list[Vec2], r: rectangle.Rectangle) -> bool:
     pass
+
+
+def star2():
+    tiles = [Vec2(x, y) for x, y in read(2025, 9, sep=',')]
+    result = 0
+    for u, v in combinations(tiles, 2):
+        r = rectangle.Rectangle.by_corners(u, v)
+        if is_inside(tiles, r):
+            result = max(result, (r.width+1)*(r.height+1))
+    return result
